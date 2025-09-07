87cb2b0768e4ae275da131bad247de210a19b666
eisbaer1832 <eisbaer1832@gmail.com>
2025-09-07 22:23:11 +0200

    added cancel notifications

--- app/src/main/java/com/example/indiwarenative/data/backend/worker.kt
+++ app/src/main/java/com/example/indiwarenative/data/backend/worker.kt
@@ -5,29 +5,33 @@ import android.R
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.graphics.drawable.Icon
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.annotation.RequiresPermission
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled._7k
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.work.CoroutineWorker
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
-import androidx.work.WorkRequest
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.indiwarenative.data.DataSharer.FilterClass
 import com.example.indiwarenative.data.UserSettings
-import kotlinx.coroutines.runBlocking
+import com.example.indiwarenative.data.lesson
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.flow.first
 import java.time.LocalDate
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.util.concurrent.TimeUnit
-import kotlin.random.Random
 
 
 
@@ -45,24 +49,52 @@ fun registerWorker() {
 }
 
 
-class NotificationWorker(appContext: Context, workerParams: WorkerParameters):
-    Worker(appContext, workerParams) {
+class NotificationWorker(context: Context, workerParams: WorkerParameters):
+    CoroutineWorker(context, workerParams) {
 
 
     @RequiresApi(Build.VERSION_CODES.O)
     @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
-    override fun doWork(): Result {
+    override suspend fun doWork(): Result {
         val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
         var current = LocalDate.now()
-        var currentAsString = current.format(formatter)
+        val timeNow = LocalTime.now()
+        current = fixDay(timeNow, current)
+        val userSettings = UserSettings.getInstance(applicationContext)
+        val status: Map<String, Boolean> = userSettings.ownSubjects.first()
+        var week =  arrayListOf<ArrayList<lesson>>()
 
-        //val lessons = getLessons(userSettings, "/mobil/mobdaten/PlanKl${currentAsString}.xml")
+        for (i in 0..4) {
+            val currentAsString = current.format(formatter)
+            val lesson =
+                getLessons(
+                    userSettings,
+                    "" + "/mobil/mobdaten/PlanKl${currentAsString}.xml"
+                )
+            if (lesson != null) {
+                week.add(lesson)
+                current = current.plusDays(1)
+            }
 
-        println("executing BG Task")
-        sendNotification(
-            title = "Reminder",
-            message = "This is your scheduled notification!"
-        )
+        }
+        for (day in week) {
+            var lessons = day.filter { lesson ->
+                val key = lesson.subject.substringBefore(" ")
+                status[key] == true || (!lesson.subject.contains(Regex("\\d")) && FilterClass != "13")
+            } as ArrayList<lesson>
+            println("lessons" + lessons.joinToString())
+            for (lesson in lessons) {
+                println("lesson $lesson")
+                if (lesson.canceled) {
+                    sendNotification(
+                        title = "Entfall!",
+                        message = "${lesson.subject} fällt aus \uD83C\uDF89"
+                    )
+                }
+
+            }
+        }
+
         return Result.success()
     }
 
