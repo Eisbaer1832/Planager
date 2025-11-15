33566c944afbaf384dad4ed6ca473becb3cf5b97
eisbaer1832 <eisbaer1832@gmail.com>
2025-11-15 23:27:13 +0100

    converted to merge friendly format

--- app/src/main/java/com/capputinodevelopment/planager/components/Credentials.kt
+++
@@ -1,7 +0,0 @@
-package com.capputinodevelopment.planager.components
-
-data class Credentials (
-    var schoolID: String,
-    var username: String,
-    var password: String,
-)
--- app/src/main/java/com/capputinodevelopment/planager/data/research/SearchObject.kt
+++
@@ -1,8 +0,0 @@
-package com.capputinodevelopment.planager.data.research
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class SearchObject (
-    val name: String,
-    val icon: ImageVector
-)
